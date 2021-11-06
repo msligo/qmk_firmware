75239a21d7a0e8c4c302b0ce502f7333fc3d10b0
Michael Sligo <michael.sligo@saberastro.com>
2021-11-06 22:25:32 +1030

    The base keymap files from Oryx

---
+++ keyboards/moonlander/keymaps/thumb/config.h
@@ -0,0 +1,13 @@
+/*
+  Set any config.h overrides for your specific keymap here.
+  See config.h options at https://docs.qmk.fm/#/config_options?id=the-configh-file
+*/
+#define ORYX_CONFIGURATOR
+#define PERMISSIVE_HOLD
+
+#undef IGNORE_MOD_TAP_INTERRUPT
+
+#define USB_SUSPEND_WAKEUP_DELAY 0
+#define CAPS_LOCK_STATUS
+#define FIRMWARE_VERSION u8"OYbYa/WRDPP"
+#define RGB_MATRIX_STARTUP_SPD 60
